fe5e3eae925fc0878499177c5d2640fda1253ea9
zhengqian <zhengqian_c@outlook.com>
2024-03-30 22:29:16 +0100

    Update with my own pics

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ const frameMetadata = getFrameMetadata({
       label: "Begin"
     }
   ],
-  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/0.png`,
-  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmVbQpJ3fEDQDHSMqcakrCkkzzMBVCwTFnKHJeBXkDBMVs/1.png`,
+  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=2`,
 });
 
 export const metadata: Metadata = {
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Cosmic Cowboys',
     description: 'A frame telling the story of Cosmic Cowboys',
-    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/Qme4FXhoxHHfyzTfRxSpASbMF8kajLEPkRQWhwWu9pkUjm/0.png`],
+    images: [`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmVbQpJ3fEDQDHSMqcakrCkkzzMBVCwTFnKHJeBXkDBMVs/1.png`],
   },
   other: {
     ...frameMetadata,
